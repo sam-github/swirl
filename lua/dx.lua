bff21f56f81fb11ec54ead73aaf28365a078ade7
sam <sam@30377ac7-81ed-0310-b95c-ca3397ef9124>
2006-12-02 05:22:36 +0000

    debug example
    git-svn-id: file:///Shared/.Subversion/chat/trunk@437 30377ac7-81ed-0310-b95c-ca3397ef9124

---
+++ lua/dx.lua
@@ -0,0 +1,49 @@
+io.stdout:setvbuf"line"
+
+require"vortex"
+
+print("register: ".."http://beep.ensembleindependant.org/profiles/chat")
+
+vortex:profiles_register{ 
+  profile = "http://beep.ensembleindependant.org/profiles/chat",
+  start = function (profile, channum, conn, server, name, encoding)
+    print(
+      "start"..
+      " #"..channum..
+      " profile="..profile..
+      " server="..tostring(server)..
+      " content="..name..
+      " encoding="..encoding
+    )
+    --vortex:listener_unlock()
+    return true
+  end,
+  close = function (...)
+    --vortex:listener_unlock()
+    return true
+  end,
+  frame = function (chan, conn, frame)
+    print("frame#"..frame:msgno()..":"..frame:type().."<"..frame:payload()..">")
+    chan:send_rpy(frame:msgno(), "ok")
+    if frame:payload() == "x" then
+      print"..."
+      print("conn:close() -> "..conn:close())
+      vortex:listener_unlock()
+      print"vortex:listener_unlock()"
+    end
+  end,
+}
+
+vortex:listener_new{port=44000}
+
+print"listener wait.."
+
+vortex:listener_wait()
+
+print"listener woke"
+
+vortex:exit()
+
+print"exited"
+
+-- vim:ft=lua:
