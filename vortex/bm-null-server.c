934a77f6aac3f1ae9b10374be1aa89194f434bd7
Sam Roberts <vieuxtech@gmail.com>
2008-11-05 20:19:23 -0800

    deleted old and dead vortex code, added null benchmarks (untested, don't have a working vortex)

--- vortex/bm-null-server.c
+++ vortex/bm-null-server.c
@@ -1,79 +1,85 @@
 #include <vortex.h>
 
-#define NULL_PROFILE "http://example.com/BEEP/NULL"
+#define NULL_PROFILE "http://example.com/beep/null"
 
 int count = 0;
 
 void frame_received (VortexChannel * channel,
-        VortexConnection * connection,
-        VortexFrame * frame,
-        axlPointer user_data)
+    VortexConnection * connection,
+    VortexFrame * frame,
+    axlPointer user_data)
 {
-    printf ("A frame received on channl: %d\n", vortex_channel_get_number (channel));
-    printf ("Data received: [%d] '%s'\n",
-            vortex_frame_get_payload_size (frame),
-            vortex_frame_get_payload (frame));
+  printf ("A frame received on channl: %d\n", vortex_channel_get_number (channel));
+  printf ("Data received: [%d] '%s'\n",
+      vortex_frame_get_payload_size (frame),
+      vortex_frame_get_payload (frame));
 
-    /* reply the peer client with the same content */
-    vortex_channel_send_rpyv (channel,
-            vortex_frame_get_msgno (frame),
-            "%d",
-            count);
+  /* reply the peer client with the same content */
+  vortex_channel_send_rpyv (channel,
+      vortex_frame_get_msgno (frame),
+      "%d",
+      count);
 
-    printf ("VORTEX_LISTENER: end task (pid: %d)\n", getpid ());
+  printf ("VORTEX_LISTENER: end task (pid: %d)\n", getpid ());
 
-    return;
+  return;
 }
 
 bool start_channel (int channel_num, 
-        VortexConnection * connection, 
-        axlPointer user_data)
+    VortexConnection * connection, 
+    axlPointer user_data)
 {
-    count = 0;
-    return true;
+  count = 0;
+  return true;
 }
 
 bool close_channel (int channel_num, 
-        VortexConnection * connection, 
-        axlPointer user_data)
+    VortexConnection * connection, 
+    axlPointer user_data)
 {
-    return true;
+  return true;
 }
 
 bool on_accepted (VortexConnection * connection, axlPointer data)
 {
-    printf ("New connection accepted from: %s:%s\n", 
-            vortex_connection_get_host (connection),
-            vortex_connection_get_port (connection));
+  printf ("New connection accepted from: %s:%s\n", 
+      vortex_connection_get_host (connection),
+      vortex_connection_get_port (connection));
 
-    /* return true to accept the connection to be created */
-    return true;
+  /* return true to accept the connection to be created */
+  return true;
 }
+
+const char USAGE[] = "usage: %s <port>\n";
 
 int main (int argc, char ** argv) 
 {
+  if(argc != 2) {
+    printf(USAGE, argv[0]);
+    return 1;
+  }
 
-    /* init vortex library */
-    vortex_init ();
+  /* init vortex library */
+  vortex_init ();
 
-    /* register a profile */
-    vortex_profiles_register (NULL_PROFILE, 
-            start_channel, NULL, 
-            close_channel, NULL,
-            frame_received, NULL);
+  /* register a profile */
+  vortex_profiles_register (NULL_PROFILE, 
+      start_channel, NULL, 
+      close_channel, NULL,
+      frame_received, NULL);
 
-    /* create a vortex server */
-    vortex_listener_new ("0.0.0.0", "1976", NULL, NULL);
+  /* create a vortex server */
+  vortex_listener_new ("0.0.0.0", argv[1], NULL, NULL);
 
-    /* configure connection notification */
-    vortex_listener_set_on_connection_accepted (on_accepted, NULL);
+  /* configure connection notification */
+  vortex_listener_set_on_connection_accepted (on_accepted, NULL);
 
-    /* wait for listeners (until vortex_exit is called) */
-    vortex_listener_wait ();
+  /* wait for listeners (until vortex_exit is called) */
+  vortex_listener_wait ();
 
-    /* end vortex function */
-    vortex_exit ();
+  /* end vortex function */
+  vortex_exit ();
 
-    return 0;
+  return 0;
 }
 
